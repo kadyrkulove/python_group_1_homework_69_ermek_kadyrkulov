ff384985aec1a7e161ccffb2b09b2caf29774cd5
Ermek K <kadyrkulove@gmail.com>
2019-03-28 16:46:22 +0600

    add reduce.js, added component in App.js, modifyed css, fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {connect} from "react-redux";
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <div className='Box'>
+         <div className={this.props.isRight === true ? 'green' : 'red'}>
+           <p>{this.props.ctr}</p>
+         </div>
+          <div className='Tabs'>
+            <button onClick={() => this.props.addNumber(9)}>9</button>
+            <button onClick={() => this.props.addNumber(8)}>8</button>
+            <button onClick={() => this.props.addNumber(7)}>7</button>
+            <button onClick={() => this.props.addNumber(6)}>6</button>
+            <button onClick={() => this.props.addNumber(5)}>5</button>
+            <button onClick={() => this.props.addNumber(4)}>4</button>
+            <button onClick={() => this.props.addNumber(3)}>3</button>
+            <button onClick={() => this.props.addNumber(2)}>2</button>
+            <button onClick={() => this.props.addNumber(1)}>1</button>
+            <button onClick={this.props.clearNumber}>&lt;</button>
+            <button onClick={() => this.props.addNumber(0)}>0</button>
+            <button onClick={this.props.pressNumber}>E</button>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-export default App;
+const  mapStateToProps = state =>{
+  return{
+    ctr: state.stars,
+    isRight: state.isRight,
+  };
+};
+const mapDispatchToProps = dispatch=>{
+  return{
+    addNumber: (value)=> dispatch({type: 'ADD_NUMBER', value}),
+    pressNumber: ()=> dispatch({type: 'PRESS'}),
+    clearNumber: ()=> dispatch({type: 'CLEAR'}),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps) (App);
---
+++ src/store/Reduce.js
@@ -0,0 +1,36 @@
+const initialState = {
+  pass: '',
+  currentPass: '1234',
+  isRight: false,
+  stars: '',
+};
+
+const reducer = (state = initialState, action)=>{
+  if (action.type === 'ADD_NUMBER'){
+    if(state.pass.length < 4) {
+      return{
+        ...state,
+        pass: state.pass += action.value,
+        stars: state.stars += '*',
+      }
+    }
+  }
+  if (action.type === 'PRESS') {
+    if (state.pass === state.currentPass){
+      return {...state, stars: 'Access Granted', isRight: true}
+    }
+  }
+  if (action.type === 'CLEAR') {
+    const newPass = state.pass.substr(0, state.pass.length - 1);
+    const newStars = state.stars.substr(0, state.stars.length - 1);
+    return{
+      ...state,
+      pass: newPass,
+      stars: newStars
+    }
+  }
+
+  return state
+};
+
+export default reducer;
